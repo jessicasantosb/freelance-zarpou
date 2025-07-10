f7236b842f9dfa63bd97aa7973c9dcd0197a26b8
Jessica <jessica.santosb@outlook.com>
2025-07-10 10:54:55 -0300

    feat: initial config

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Sobre nós",
+  description:
+    "",
+};
+
+export default function About() {
+
+  return (
+    <main>
+      <h3>Sobre nós</h3>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Contato",
+  description:
+    "",
+};
+
+export default function Contact() {
+
+  return (
+    <main>
+      <h3>Contato</h3>
+    </main>
+  );
+}
