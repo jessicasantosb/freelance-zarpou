7a5e95c6c216e011b10b6ed2cc50e43c26bea030
Jessica <jessica.santosb@outlook.com>
2025-07-10 11:19:52 -0300

    feat: add footer component and integrate into layout

---
+++ components/footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export function Footer() {
+
+  return (
+    <footer className="w-full bg-primary text-white text-sm">
+      <div className="container p-2 mx-auto flex flex-col sm:flex-row justify-between gap-4 items-center">
+        <div className="flex flex-col items-center md:flex-row">
+          <p>
+            Zarpou Viagens. {' '}
+
+            <span>
+              Todos os direitos reservados.
+            </span>
+          </p>
+          <p>
+            CNPJ: 000000000 | Brasil
+          </p>
+        </div>
+
+        <p>
+          DESENVOLVIDO POR &copy;
+          {" "}
+          <Link
+            href={`${process.env.NEXT_PUBLIC_PORTFOLIO_LINK}`}
+            target="_blank"
+            className="hover:opacity-90 "
+          >
+            Jessica
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+}
