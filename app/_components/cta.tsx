f034f74bad910c4a822033e4a97ef508b998096e
Jessica <jessica.santosb@outlook.com>
2025-07-11 15:37:12 -0300

    feat: add cta

---
+++ app/_components/cta.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { PhoneCall } from "lucide-react";
+
+export function Cta() {
+  return (
+    <section className="h-[40dvh] md:h-[60dvh] grid grid-cols-1 md:grid-cols-2 mb-12 shadow-xl">
+      {/* Vídeo de Micaela Alejandro: https://www.pexels.com/pt-br/video/praia-litoral-espuma-oceano-10758010/ */}
+      <video
+        src="/cta.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="hidden md:block w-full h-[60dvh] object-cover"
+      />
+
+      <div className="flex-1 p-2 flex flex-col items-center justify-center bg-[#1e5f98] text-white text-center">
+        <h1 className="text-2xl md:text-4xl font-bold">
+          Garanta um futuro cheio de possibilidades
+        </h1>
+        <p className="py-4 text-md md:text-lg">
+          O futuro é promissor para quem se antecipa
+        </p>
+        <Button
+          className="shadow-2xl shadow-secondary cursor-pointer"
+          size={"lg"}
+        >
+          Vamos conversar
+          <PhoneCall className="size-6" />
+        </Button>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { Hero } from "@/app/_components/hero";
 import { Menu } from "./_components/menu";
 import { TopChoices } from "./_components/top-choices";
+import { Cta } from "./_components/cta";
 
 export default function Home() {
   return (
-    <main className="min-h-dvh">
+    <main>
       <Hero />
       <Menu />
       <TopChoices />
+      <Cta />
     </main>
   );
 }
