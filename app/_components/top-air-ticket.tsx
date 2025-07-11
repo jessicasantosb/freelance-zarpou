efd9b7da45e16f5bb7f1810a3d7a9943d5c8ba99
Jessica <jessica.santosb@outlook.com>
2025-07-11 12:04:56 -0300

    style: change airplane image position

--- app/_components/top-air-ticket.tsx
+++ app/_components/top-air-ticket.tsx
@@ -14,8 +14,8 @@ import Image from "next/image";
 
 export function TopAirTicket() {
   return (
-    <Container>
-      <div className="hidden md:block relative h-60 lg:h-72 w-full -ml-[25rem] lg:-ml-[30rem]">
+    <>
+      <div className="block relative h-44 md:h-60 lg:h-96 w-full -mb-20 sm:-mb-14 lg:-mb-24 -ml-[10rem] md:-ml-[25rem] lg:-ml-[30rem]">
         <Image
           alt="aircraft"
           src={"/airplane.png"}
@@ -24,36 +24,41 @@ export function TopAirTicket() {
         />
       </div>
 
-      <Title
-        title="Passagens para o Exterior"
-        subtitle="Explore novos países com praticidade e ótimos preços."
-      />
-      <div className="py-8 flex flex-col gap-6">
-        {airTickets.map(
-          ({ title, description, link, departure_from, image_url }, index) => (
-            <Card key={index} className="flex-row">
-              <div className="relative w-96 h-52">
-                <Image
-                  alt="city"
-                  src={image_url}
-                  fill
-                  className="absolute object-cover rounded-l-xl"
-                />
-              </div>
-              <CardContent>
-                <CardTitle>{title}</CardTitle>
-                <p className="text-muted-foreground flex pb-2">
-                  <Dot /> {departure_from}
-                </p>
-                <CardDescription>{description}</CardDescription>
-              </CardContent>
-              <CardFooter>
-                <CustomLink link={link} text="Ver passagens" />
-              </CardFooter>
-            </Card>
-          )
-        )}
-      </div>
-    </Container>
+      <Container>
+        <Title
+          title="Passagens para o Exterior"
+          subtitle="Explore novos países com praticidade e ótimos preços."
+        />
+        <div className="py-8 flex flex-col gap-6">
+          {airTickets.map(
+            (
+              { title, description, link, departure_from, image_url },
+              index
+            ) => (
+              <Card key={index} className="flex-row">
+                <div className="relative w-96 h-52">
+                  <Image
+                    alt="city"
+                    src={image_url}
+                    fill
+                    className="absolute object-cover rounded-l-xl"
+                  />
+                </div>
+                <CardContent>
+                  <CardTitle>{title}</CardTitle>
+                  <p className="text-muted-foreground flex pb-2">
+                    <Dot /> {departure_from}
+                  </p>
+                  <CardDescription>{description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                  <CustomLink link={link} text="Ver passagens" />
+                </CardFooter>
+              </Card>
+            )
+          )}
+        </div>
+      </Container>
+    </>
   );
 }
