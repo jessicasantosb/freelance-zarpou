6c3e19509c0e809c7427ec5026b2978db513d9e5
Jessica <jessica.santosb@outlook.com>
2025-07-12 14:00:39 -0300

    feat: add useScrollState hook to track scroll position

---
+++ hooks/useScrollState.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useScrollState = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrolled;
+};
+
