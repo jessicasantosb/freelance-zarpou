46c78cab6fa18745888a1c6f19af6d34c1ccda90
Jessica <jessica.santosb@outlook.com>
2025-07-12 14:27:30 -0300

    feat: refactor Title component to style just the first word

--- components/shared/title.tsx
+++ components/shared/title.tsx
@@ -4,13 +4,21 @@ type TitleProps = {
 };
 
 export function Title({ title, subtitle }: TitleProps) {
+   const splittedTitle = title.split(" ");
+  const [firstWord, ...rest] = splittedTitle;
+
   return (
     <div className="relative text-center">
-      <h1 className="relative inline-block text-2xl md:text-4xl font-bold">
-        <span className="hidden md:block h-2 w-24 absolute -bottom-1 -left-4 -z-10 rounded bg-primary/50" />
-        {title}
+      <h1 className="text-2xl md:text-4xl font-bold">
+        <span className="relative inline-block">
+          {firstWord}
+          <span className="h-2 w-24 absolute -bottom-1 -left-4 -z-10 rounded bg-primary/50" />
+        </span>{" "}
+        {rest.join(" ")}
       </h1>
-      <p className="pt-2 text-md md:text-lg text-muted-foreground">{subtitle}</p>
+      <p className="pt-2 text-md md:text-lg text-muted-foreground">
+        {subtitle}
+      </p>
     </div>
   );
 }
