8da1cbdc423fd1e6f05d7abccd4dc26cfc439ae4
Jessica <jessica.santosb@outlook.com>
2025-07-13 14:06:29 -0300

    feat: add links to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,29 +2,39 @@ import Link from "next/link";
 import { FooterTop } from "./footer-top";
 
 export function Footer() {
-
   return (
     <footer>
       <FooterTop />
 
-      <div className="w-full bg-primary text-white text-sm">
-        <div className="container p-2 mx-auto flex flex-col sm:flex-row justify-between gap-4 items-center">
-          <div className="flex flex-col items-center md:flex-row md:gap-4">
-            <p>
-              Zarpou Viagens e Turismo. {' '}
-
-              <span>
-                Todos os direitos reservados.
-              </span>
+      <div className="w-full bg-primary text-white text-[.8rem]">
+        <div className="container p-2 mx-auto flex flex-col sm:flex-row items-center md:items-start justify-between gap-4">
+          <div className="flex flex-col items-center">
+            <p className="flex flex-col items-center md:flex-row md:gap-4">
+              Zarpou Viagens e Turismo. Todos os direitos reservados.
+              <span>CNPJ: 000000000 | Brasil</span>
             </p>
-            <p>
-              CNPJ: 000000000 | Brasil
-            </p>
+            <div className="w-full flex justify-center md:justify-start gap-2 [&>*]:underline">
+              <Link
+                target="_blank"
+                href={
+                  "https://onertravel.com/termos-e-condicoes/?_gl=1%2A1n8zhfm%2A_gcl_au%2AMjg1NjExMDk3LjE3NTIwNjM3MzE.%2A_ga%2AMTg5NzgxNjkyNi4xNzUyMDYzNzMx%2A_ga_PNM2Q8Q1WF%2AczE3NTI0MTMxODgkbzE2JGcwJHQxNzUyNDEzMTg4JGo2MCRsMCRoMA.."
+                }
+              >
+                Termos e condições
+              </Link>
+              <Link
+                target="_blank"
+                href={
+                  "https://onertravel.com/politica-de-privacidade/?_gl=1%2A1n8zhfm%2A_gcl_au%2AMjg1NjExMDk3LjE3NTIwNjM3MzE.%2A_ga%2AMTg5NzgxNjkyNi4xNzUyMDYzNzMx%2A_ga_PNM2Q8Q1WF%2AczE3NTI0MTMxODgkbzE2JGcwJHQxNzUyNDEzMTg4JGo2MCRsMCRoMA.."
+                }
+              >
+                Política de Privacidade
+              </Link>
+            </div>
           </div>
 
           <p>
-            DESENVOLVIDO POR &copy;
-            {" "}
+            DESENVOLVIDO POR &copy;{" "}
             <Link
               href={`${process.env.NEXT_PUBLIC_PORTFOLIO_LINK}`}
               target="_blank"
