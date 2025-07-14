ac0926b2ada4c630f133b3a5cf185420ef4ee9d8
Jessica <jessica.santosb@outlook.com>
2025-07-14 20:24:42 -0300

    feat: change menu icons

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 import { Container } from "@/components/shared/container";
 import { Title } from "@/components/shared/title";
-import { Building, Car, Luggage, Plane, Ticket } from "lucide-react";
-import Image from "next/image";
+
+type MenuIconProps = {
+  src: string;
+  text: string;
+};
 
 export function Menu() {
   return (
@@ -11,23 +16,13 @@ export function Menu() {
         subtitle="Organize toda a sua viagem com praticidade e confiança"
       />
       <div className="pb-8 flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 lg:min-h-[60dvh]">
-        <div className="pt-12 text-xl md:text-2xl text-left text-[#393838] flex flex-col leading-14 [&>*]:flex [&>*]:items-center [&>*]:gap-2 [&_svg]:text-secondary [&_svg]:size-10">
-          <p>
-            <Plane /> Passagem de avião
-          </p>
-          <p>
-            <Building /> Hospedagem
-          </p>
-          <p>
-            <Luggage /> Pacote de viagem completo
-          </p>
-          <p>
-            <Ticket /> Ingresso para shows
-          </p>
-          <p>
-            <Car /> Aluguel de carros
-          </p>
-        </div>
+        <ul className="pt-12 text-xl md:text-2xl text-left text-[#393838] flex flex-col leading-14">
+          <MenuIcon src="/icons/plane.svg" text="Passagem de avião" />
+          <MenuIcon src="/icons/building.svg" text="Hospedagem" />
+          <MenuIcon src="/icons/luggage.svg" text="Pacote de viagem completo" />
+          <MenuIcon src="/icons/ticket.svg" text="Ingresso para shows" />
+          <MenuIcon src="/icons/car.svg" text="Aluguel de carros" />
+        </ul>
 
         <div className="hidden lg:flex flex-col">
           <div className="relative size-[30rem] flex flex-col">
@@ -51,3 +46,14 @@ export function Menu() {
     </Container>
   );
 }
+
+const MenuIcon = ({ src, text }: MenuIconProps) => {
+  return (
+    <li className="flex items-center gap-4 py-2">
+      <div className="relative size-14 flex flex-col">
+        <Image alt={text} src={src} fill className="absolute object-cover" />
+      </div>
+      <p className="text-black/80">{text}</p>
+    </li>
+  );
+};
